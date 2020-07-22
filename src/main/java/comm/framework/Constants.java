486626755f7b2b33acd106552e127afc27c64e9d
Ajay Kushwaha <ajay.kushwaha@olacabs.com>
2020-07-23 00:50:20 +0530

    Commit1

--- src/main/java/comm/framework/Constants.java
+++ src/main/java/comm/framework/Constants.java
@@ -5,5 +5,8 @@ public class Constants {
     public static final String CONFIG_FILE_PATH ="./src/main/java/comm/config/config.properties" ;
     public static final String CHROMEDRIVER_PATH = "./src/main/resources/chromedriver";
     public static final String CHROME_KEY = "webdriver.chrome.driver";
+    public static final String CHROME = "chrome";
+    public static final String BROWSER = "browser";
+
 
 }
--- src/main/java/comm/framework/TestBaseTest.java
+++ src/main/java/comm/framework/TestBaseTest.java
@@ -1,21 +1,30 @@
 package comm.framework;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeSuite;
 
 public class TestBaseTest {
     private static TestBasePage testBasePage;
     private static String browserValue;
+    public static WebDriver driver;
 
 
     @BeforeSuite
     public void initialSetup(){
         testBasePage = new TestBasePage();
-        this.browserValue = testBasePage.getConfigValue("browse");
+        this.browserValue = testBasePage.getConfigValue(Constants.BROWSER);
+        webDriverInitialization();
     }
 
     public static void webDriverInitialization(){
-        if(browserValue.equalsIgnoreCase("chrome")){
+        if(browserValue.equalsIgnoreCase(Constants.CHROME)){
             System.setProperty(Constants.CHROME_KEY,Constants.CHROMEDRIVER_PATH);
+            driver = new ChromeDriver();
         }
+    }
+
+    public WebDriver getdriver(){
+        return driver;
     }
 }
